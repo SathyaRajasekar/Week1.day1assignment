c17224cc2a9f7ce188702836230b9649fdaa5e0f
SathyaRajasekar <104005937+SathyaRajasekar@users.noreply.github.com>
2022-04-21 11:00:47 +0530

    Add files via upload

---
+++ AprWeek1Assignments/Calculator.java
@@ -0,0 +1,38 @@
+package week1.day1.AprWeek1Assignments;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		//System.out.println("Addition="  'l+ m + n);
+		Calculator cal1=new Calculator();
+		cal1.sub(10,5);
+		
+		//System.out.println(k);
+	}
+	
+		// TODO Auto-generated method stu
+public void add(int l  , int m, int n)
+{
+	l=2;
+	m=5;
+	n=4;
+}
+public void sub(int i, int j) 
+{
+	//i=10;
+	//j=5;
+	int k;
+	k=i-j;
+	System.out.println(k);
+	
+}
+public void mul(int i, int j) 
+{
+	i=10;
+	j=5;
+}
+public void divide(double num1, double num2) {
+	num1=20.5;
+	num2=5.0;
+}
+}
---
+++ AprWeek1Assignments/test.java
@@ -0,0 +1,19 @@
+package week1.day1.AprWeek1Assignments;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//int var1=5;
+//int Var2=6;
+//if ((Var2 = 1)== var1)
+//{System.out.println(Var2);
+	//}
+//else {
+	//System.out.println(++Var2);
+		int temp=0;
+		for(int i=1;i<=10;i++) 
+			temp+=i;
+		System.out.println(temp);
+}
+}
