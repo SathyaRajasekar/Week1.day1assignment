9014aaebb8f877d464daac933ac826f1edbd9cba
SathyaRajasekar <104005937+SathyaRajasekar@users.noreply.github.com>
2022-05-06 11:56:11 +0530

    Add files via upload

---
+++ NegativeNumberToPositiveNumber.java
@@ -0,0 +1,14 @@
+package firstweekLearnDataTypes;
+
+public class NegativeNumberToPositiveNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = -40;
+		if(number<0) {
+			number=number*-1;
+			System.out.println("the number -40 is converted to" +number);
+		}
+	}
+
+}
---
+++ NumberPositiveorNegative.java
@@ -0,0 +1,16 @@
+package firstweekLearnDataTypes;
+
+public class NumberPositiveorNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int number= -35;;
+if(number>0) {
+	System.out.println("The given number is postive");
+
+} if(number<0){
+	System.out.println("The given number is negative");
+}
+	}
+
+}
